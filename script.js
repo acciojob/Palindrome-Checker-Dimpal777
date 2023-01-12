d7130b2ee71fc09639e5db3620bdf8757ad5e2ac
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-01-12 23:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
+function palindrome(str) {
+  // Good luck!
+  /* remove special characters, spaces and make lowercase*/
+  var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
-function palindrome(str){
+  /* reverse removeChar for comparison*/
+  var checkPalindrome = removeChar.split('').reverse().join('');
 
+  /* Check to see if str is a Palindrome*/
+   if(removeChar === checkPalindrome){
+     return true;
+   }else{
+     return false;
+   }
 }
-module.exports = palindrome
+
+
+
+palindrome("eye");
